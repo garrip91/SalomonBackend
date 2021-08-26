78c94d2d01229b25fc3b1fb8cdcf174edcf87cc0
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-08-26 19:31:46 +0300

    Update dashboard.py

--- src/main/dashboard.py
+++ src/main/dashboard.py
@@ -4,20 +4,20 @@ from jet.dashboard.dashboard import Dashboard, AppIndexDashboard
 from django.utils.translation import ugettext_lazy as _
 
 
-class CustomIndexDashboard(Dashboard):
-    columns = 1
+# class CustomIndexDashboard(Dashboard):
+    # columns = 1
 
-    def init_with_context(self, context):
-        self.available_children.append(modules.LinkList)
-        self.children.append(modules.LinkList(
-            _('Поддержка'),
-            children=[
-                {
-                    'title': _('При возникновении проблем писать'),
-                    'url': 'Тут будет ссылка',
-                    'external': True,
-                },
-            ],
-            column=0,
-            order=0
-        ))
+    # def init_with_context(self, context):
+        # self.available_children.append(modules.LinkList)
+        # self.children.append(modules.LinkList(
+            # _('Поддержка'),
+            # children=[
+                # {
+                    # 'title': _('При возникновении проблем писать'),
+                    # 'url': 'Тут будет ссылка',
+                    # 'external': True,
+                # },
+            # ],
+            # column=0,
+            # order=0
+        # ))
